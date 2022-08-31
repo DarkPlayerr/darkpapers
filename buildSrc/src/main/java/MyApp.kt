60cfc7218f2bb0d4d4b89195243b6452a51ee9a7
DarkPlayer <darek.duong@gmail.com>
2022-08-31 23:36:19 +0200

    darkpapers: update app version

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.dark.papers"
-    const val version = 111
-    const val versionName = "1.1.1"
+    const val version = 112
+    const val versionName = "1.1.2"
 }
